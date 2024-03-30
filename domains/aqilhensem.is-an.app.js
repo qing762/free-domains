c063d5a57dd9a7a6e1f0b0d5d0209c9790ef933d
qing. <threatedblade@outlook.com>
2024-03-31 07:26:17 +0800

    Create aqilhensem.is-an.app.js

---
+++ domains/aqilhensem.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Funny website to help my friend with their school project.',
+  domain: 'is-an.app',
+  subdomain: 'qell',
+  owner: {
+    repo: 'https://github.com/qing762/qing762.github.io',
+    email: 'threatedblade@outlook.com',
+  },
+  record: {
+    CNAME: 'https://qing762.github.io/',
+  },
+})
